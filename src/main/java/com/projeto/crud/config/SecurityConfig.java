efe833c7fb816c3f55adabeaa0856c8a0292bab8
vitorgabripr <vitor.gabrielll0245@gmail.com>
2025-09-09 15:31:57 -0300

    adicionando interface

---
+++ src/main/java/com/projeto/crud/config/SecurityConfig.java
@@ -0,0 +1,4 @@
+package com.projeto.crud.config;
+
+public class SecurityConfig {
+}
--- src/main/java/com/projeto/crud/model/Model.java
+++ src/main/java/com/projeto/crud/model/Model.java
@@ -1,17 +1,17 @@
 package com.projeto.crud.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class Model {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+    private String email;
+    private String senha;
 
     public Long getId() {
         return id;
@@ -27,6 +27,22 @@ public class Model {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
     }
 
     public void updateName(String nome) {
